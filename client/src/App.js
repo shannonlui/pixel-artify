6206665952c98cbf8a6007c2790a7c4c5cd4c3cb
shannonlui <shannonlui@hotmail.com>
2019-05-09 12:58:53 -0400

    Add router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
+import Canvas from './containers/Canvas/Canvas';
 
 function App() {
+  let routes = (
+    <Switch>
+      <Route path="/" exact component={Canvas} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
     <div>
       <Layout>
+        {routes}
       </Layout>
     </div>
   );
