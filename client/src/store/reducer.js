cf7ca65d7f425d9431f4a6d7b8944a5ebf9a6e87
shannonlui <shannonlui@hotmail.com>
2022-05-27 20:31:18 -0400

    Refactored reducer logic

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionTypes';
-import { updateObject, isDevEnv } from '../utils/utility';
+import { updateObject, createReducer, isDevEnv } from '../utils/utility';
 import testImg from '../assets/images/car.png';
 import { TOOL_TYPES } from '../constants/constants';
 
@@ -25,34 +25,60 @@ const initialState = {
   toolType: TOOL_TYPES.PAINT,
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.LOAD_IMAGE:
-      return updateObject(initialState, {image: action.image, loading: true});
-    case actionTypes.LOAD_IMAGE_SUCCESS:
-      return updateObject(state, {loading: false});
-    case actionTypes.UPDATE_PIXEL_SIZE:
-      return updateObject(state, {pixelSize: action.pixelSize});
-    case actionTypes.UPDATE_CONTRAST:
-      return updateObject(state, {contrast: action.contrast});
-    case actionTypes.UPDATE_BRIGHTNESS:
-      return updateObject(state, {brightness: action.brightness});
-    case actionTypes.UPDATE_SATURATION:
-      return updateObject(state, {saturation: action.saturation});
-    case actionTypes.UPDATE_COLOR_COUNT:
-      return updateObject(state, {
-        colorCount: action.colorCount, 
-        colorPalette: action.colorPalette
-      });
-    case actionTypes.ENABLE_PAINT:
-      return updateObject(state, {isPaintEnabled: true});
-    case actionTypes.UPDATE_PAINT_COLOR:
-      return updateObject(state, {paintColor: action.paintColor});
-    case actionTypes.UPDATE_TOOL_TYPE:
-      return updateObject(state, {toolType: action.toolType});
-    default:
-      return state;
-  }
-};
+function loadImage(state, action) {
+  return updateObject(initialState, {image: action.image, loading: true});
+}
+
+function loadImageSuccess(state, action) {
+  return updateObject(state, {loading: false});
+}
+
+function enablePaint(state, action) {
+  return updateObject(state, {isPaintEnabled: true});
+}
+
+function updatePixelSize(state, action) {
+  return updateObject(state, {pixelSize: action.pixelSize});
+}
+
+function updateContrast(state, action) {
+  return updateObject(state, {contrast: action.contrast});
+}
+
+function updateBrightness(state, action) {
+  return updateObject(state, {brightness: action.brightness});
+}
+
+function updateSaturation(state, action) {
+  return updateObject(state, {saturation: action.saturation});
+}
+
+function updateColorCount(state, action) {
+  return updateObject(state, {
+    colorCount: action.colorCount, 
+    colorPalette: action.colorPalette
+  });
+}
+
+function updatePaintColor(state, action) {
+  return updateObject(state, {paintColor: action.paintColor});
+}
+
+function updateToolType(state, action) {
+  return updateObject(state, {toolType: action.toolType});
+}
+  
+const reducer = createReducer(initialState, {
+  [actionTypes.LOAD_IMAGE]: loadImage,
+  [actionTypes.LOAD_IMAGE_SUCCESS]: loadImageSuccess,
+  [actionTypes.ENABLE_PAINT]: enablePaint,
+  [actionTypes.UPDATE_PIXEL_SIZE]: updatePixelSize,
+  [actionTypes.UPDATE_CONTRAST]: updateContrast,
+  [actionTypes.UPDATE_BRIGHTNESS]: updateBrightness,
+  [actionTypes.UPDATE_SATURATION]: updateSaturation,
+  [actionTypes.UPDATE_COLOR_COUNT]: updateColorCount,
+  [actionTypes.UPDATE_PAINT_COLOR]: updatePaintColor,
+  [actionTypes.UPDATE_TOOL_TYPE]: updateToolType,
+});
 
 export default reducer;
--- client/src/utils/utility.js
+++ client/src/utils/utility.js
@@ -5,4 +5,15 @@ export const updateObject = (oldObject, updatedValues) => {
   }
 };
 
+// https://redux.js.org/usage/structuring-reducers/refactoring-reducer-example
+export function createReducer(initialState, handlers) {
+  return function reducer(state = initialState, action) {
+    if (handlers.hasOwnProperty(action.type)) {
+      return handlers[action.type](state, action)
+    } else {
+      return state
+    }
+  }
+}
+
 export const isDevEnv = () => process.env.NODE_ENV === 'development';
